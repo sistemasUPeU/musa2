d266147eaaf05901af9342a0203d3ad5973a1b1b
nickmeza <nickmeza@upeu.edu.pe>
2019-10-30 13:45:39 -0500

    hola

--- src/main/java/com/musa2/entity/Vinculos.java
+++ src/main/java/com/musa2/entity/Vinculos.java
@@ -16,6 +16,146 @@ public class Vinculos {
 	private int idempleado;
 	private int idvehiculo;
 	
+	public int getIdvinculo() {
+		return idvinculo;
+	}
+
+
+	public void setIdvinculo(int idvinculo) {
+		this.idvinculo = idvinculo;
+	}
+
+
+	public int getTipovinculo() {
+		return tipovinculo;
+	}
+
+
+	public void setTipovinculo(int tipovinculo) {
+		this.tipovinculo = tipovinculo;
+	}
+
+
+	public int getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+
+
+	public int getIdconductor() {
+		return idconductor;
+	}
+
+
+	public void setIdconductor(int idconductor) {
+		this.idconductor = idconductor;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public String getFechainicio() {
+		return fechainicio;
+	}
+
+
+	public void setFechainicio(String fechainicio) {
+		this.fechainicio = fechainicio;
+	}
+
+
+	public String getFechafin() {
+		return fechafin;
+	}
+
+
+	public void setFechafin(String fechafin) {
+		this.fechafin = fechafin;
+	}
+
+
+	public String getUsercreate() {
+		return usercreate;
+	}
+
+
+	public void setUsercreate(String usercreate) {
+		this.usercreate = usercreate;
+	}
+
+
+	public String getDatecreate() {
+		return datecreate;
+	}
+
+
+	public void setDatecreate(String datecreate) {
+		this.datecreate = datecreate;
+	}
+
+
+	public String getUsermodify() {
+		return usermodify;
+	}
+
+
+	public void setUsermodify(String usermodify) {
+		this.usermodify = usermodify;
+	}
+
+
+	public String getDatemodify() {
+		return datemodify;
+	}
+
+
+	public void setDatemodify(String datemodify) {
+		this.datemodify = datemodify;
+	}
+
+
+	public int getIdpropietario() {
+		return idpropietario;
+	}
+
+
+	public void setIdpropietario(int idpropietario) {
+		this.idpropietario = idpropietario;
+	}
+
+
+	public int getIdempleado() {
+		return idempleado;
+	}
+
+
+	public void setIdempleado(int idempleado) {
+		this.idempleado = idempleado;
+	}
+
+
+	public int getIdvehiculo() {
+		return idvehiculo;
+	}
+
+
+	public void setIdvehiculo(int idvehiculo) {
+		this.idvehiculo = idvehiculo;
+	}
+
+
 	public Vinculos() {
 		super();
 	}
