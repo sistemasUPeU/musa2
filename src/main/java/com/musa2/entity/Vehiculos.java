aba448438b09ac0bb15152b8f79e56a4bede21f2
Superman14 <alejaguti2015@gmail.com>
2019-10-30 01:59:11 -0500

    Se agregó vehiculos

---
+++ src/main/java/com/musa2/entity/Vehiculos.java
@@ -0,0 +1,142 @@
+package com.musa2.entity;
+
+public class Vehiculos {
+    private int idvehiculo;
+    private int nropadron;
+    private int estado;
+    private String placa;
+    private String motor;
+    private int serie;
+    private String anhofabricacion;
+    private int pasajerossentados;
+    private int pasajerospie;
+    private int idvehmodelo;
+    private int idvehmarca;
+    private int idvehcategoria;
+    private String uso;
+    private String usercreate;
+    private String usermodify;
+    private String datemodify;
+	public Vehiculos() {
+		super();
+	}
+	public Vehiculos(int idvehiculo, int nropadron, int estado, String placa, String motor, int serie,
+			String anhofabricacion, int pasajerossentados, int pasajerospie, int idvehmodelo, int idvehmarca,
+			int idvehcategoria, String uso, String usercreate, String usermodify, String datemodify) {
+		super();
+		this.idvehiculo = idvehiculo;
+		this.nropadron = nropadron;
+		this.estado = estado;
+		this.placa = placa;
+		this.motor = motor;
+		this.serie = serie;
+		this.anhofabricacion = anhofabricacion;
+		this.pasajerossentados = pasajerossentados;
+		this.pasajerospie = pasajerospie;
+		this.idvehmodelo = idvehmodelo;
+		this.idvehmarca = idvehmarca;
+		this.idvehcategoria = idvehcategoria;
+		this.uso = uso;
+		this.usercreate = usercreate;
+		this.usermodify = usermodify;
+		this.datemodify = datemodify;
+	}
+	public int getIdvehiculo() {
+		return idvehiculo;
+	}
+	public void setIdvehiculo(int idvehiculo) {
+		this.idvehiculo = idvehiculo;
+	}
+	public int getNropadron() {
+		return nropadron;
+	}
+	public void setNropadron(int nropadron) {
+		this.nropadron = nropadron;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getMotor() {
+		return motor;
+	}
+	public void setMotor(String motor) {
+		this.motor = motor;
+	}
+	public int getSerie() {
+		return serie;
+	}
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	public String getAnhofabricacion() {
+		return anhofabricacion;
+	}
+	public void setAnhofabricacion(String anhofabricacion) {
+		this.anhofabricacion = anhofabricacion;
+	}
+	public int getPasajerossentados() {
+		return pasajerossentados;
+	}
+	public void setPasajerossentados(int pasajerossentados) {
+		this.pasajerossentados = pasajerossentados;
+	}
+	public int getPasajerospie() {
+		return pasajerospie;
+	}
+	public void setPasajerospie(int pasajerospie) {
+		this.pasajerospie = pasajerospie;
+	}
+	public int getIdvehmodelo() {
+		return idvehmodelo;
+	}
+	public void setIdvehmodelo(int idvehmodelo) {
+		this.idvehmodelo = idvehmodelo;
+	}
+	public int getIdvehmarca() {
+		return idvehmarca;
+	}
+	public void setIdvehmarca(int idvehmarca) {
+		this.idvehmarca = idvehmarca;
+	}
+	public int getIdvehcategoria() {
+		return idvehcategoria;
+	}
+	public void setIdvehcategoria(int idvehcategoria) {
+		this.idvehcategoria = idvehcategoria;
+	}
+	public String getUso() {
+		return uso;
+	}
+	public void setUso(String uso) {
+		this.uso = uso;
+	}
+	public String getUsercreate() {
+		return usercreate;
+	}
+	public void setUsercreate(String usercreate) {
+		this.usercreate = usercreate;
+	}
+	public String getUsermodify() {
+		return usermodify;
+	}
+	public void setUsermodify(String usermodify) {
+		this.usermodify = usermodify;
+	}
+	public String getDatemodify() {
+		return datemodify;
+	}
+	public void setDatemodify(String datemodify) {
+		this.datemodify = datemodify;
+	}
+    
+    
+}
