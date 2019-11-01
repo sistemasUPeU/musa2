a5754fe8bb6ea8190975a4ac6f841fd4b3391546
Carrizane <miguelcarrizo@upeu.edu.pe>
2019-11-01 01:36:22 -0500

    acciones de mantenimiento v1

--- src/main/java/com/musa2/daoImp/Mant_AccionesDaoImp.java
+++ src/main/java/com/musa2/daoImp/Mant_AccionesDaoImp.java
@@ -3,37 +3,33 @@ package com.musa2.daoImp;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+
 import com.musa2.dao.Mant_AccionesDao;
 import com.musa2.entity.Mant_Acciones;
 
 public class Mant_AccionesDaoImp implements Mant_AccionesDao {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	private SimpleJdbcCall simpleJdbcCall;
+	
 	@Override
-	public int create(Mant_Acciones m) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int update(Mant_Acciones m) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int delete(int id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Map<String, Object> read(int id) {
+	public Map<String, Object> create(Mant_Acciones m) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Map<String, Object>> readAll() {
+	public Map<String, Object> update(Mant_Acciones m) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> updateState(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
