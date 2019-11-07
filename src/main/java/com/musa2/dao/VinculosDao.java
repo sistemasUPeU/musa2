a002d9cdefe86748a09ef09ef930b86f4dc0b41b
unknown <richeti1998@gmail.com>
2019-11-07 02:28:41 -0500

    modificando vinculo

--- src/main/java/com/musa2/dao/VinculosDao.java
+++ src/main/java/com/musa2/dao/VinculosDao.java
@@ -1,13 +1,17 @@
 package com.musa2.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import com.musa2.entity.Vinculos;
 
 public interface VinculosDao {
-	public int create(Vinculos vinculo);
-	public int update(Vinculos vinculo);
-	public int delete(int id);
+	public Map<String, Object> create(Vinculos vinculo); 
+    public Map<String, Object> update(Vinculos vinculo);
+    public Map<String, Object> updateState(Vinculos vinculo);
 	public Map<String, Object> read(int id);
 	public Map<String, Object> readAll(int id);
+	
+	
+   
 }
--- src/main/java/com/musa2/service/VinculosService.java
+++ src/main/java/com/musa2/service/VinculosService.java
@@ -5,9 +5,9 @@ import java.util.Map;
 import com.musa2.entity.Vinculos;
 
 public interface VinculosService {
-	public int create(Vinculos vinculo);
-	public int update(Vinculos vinculo);
-	public int delete(int id);
+	public Map<String, Object> create(Vinculos vinculo); 
+    public Map<String, Object> update(Vinculos vinculo);
+    public Map<String, Object> updateState(Vinculos vinculo);
 	public Map<String, Object> read(int id);
 	public Map<String, Object> readAll(int id);
 }
