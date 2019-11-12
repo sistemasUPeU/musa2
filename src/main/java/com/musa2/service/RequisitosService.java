c62dd9836ca3beacfa973c488794fb89dd35ecf7
Carrizane <miguelcarrizo@upeu.edu.pe>
2019-11-12 01:26:47 -0500

    a ver p

--- src/main/java/com/musa2/service/RequisitosService.java
+++ src/main/java/com/musa2/service/RequisitosService.java
@@ -9,18 +9,8 @@ public interface RequisitosService {
 	public  Map<String, Object> update(Requisitos r);
 	public int delete(int id);
 	public Map<String,Object> read(int id);
-<<<<<<< HEAD
-
 	public Map<String,Object> readAll();
-
 	public Map<String,Object> lista();
-	
 	// Esto es para vinculo no borrar //
 	public Map<String,Object> list(int tipo);
-
-=======
-	public Map<String,Object> readAll();
-	// Esto es para vinculo no borrar //
-	public Map<String,Object> list(int tipo);
->>>>>>> 9485c9dff05dd3a546aa00b757fb4ee48256f3cb
 }
