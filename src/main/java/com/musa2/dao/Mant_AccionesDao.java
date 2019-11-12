81822a2ea97aee2610fbd9f229da3d1af8162b6c
Carrizane <miguelcarrizo@upeu.edu.pe>
2019-11-12 05:39:59 -0500

    tampoco se

--- src/main/java/com/musa2/dao/Mant_AccionesDao.java
+++ src/main/java/com/musa2/dao/Mant_AccionesDao.java
@@ -7,7 +7,8 @@ import java.util.Map;
 public interface Mant_AccionesDao {
 	
 	public Map<String, Object> create(Mant_Acciones m);
-    public Map<String, Object> update(Mant_Acciones m);
+    public Map<String, Object> update(int id, Mant_Acciones m);
     public Map<String, Object> updateState(int id);
     public Map<String, Object> readAllByType(int type);
+    public Map<String, Object> readById(int id);
 }
