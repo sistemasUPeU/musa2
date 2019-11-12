b65b75cb258536777ece437c4e26d474d15a9ff8
mariollori <mariollori@upeu.edu.pe>
2019-11-12 00:20:25 -0500

    trabajando

--- src/main/java/com/musa2/dao/Tarj_CirculacionDao.java
+++ src/main/java/com/musa2/dao/Tarj_CirculacionDao.java
@@ -7,7 +7,12 @@ import com.musa2.entity.Tarj_Circulacion;
 public interface Tarj_CirculacionDao {
 	public Map<String, Object> create(Tarj_Circulacion tar);
 	public Map<String, Object> update(Tarj_Circulacion tar);
-    public Map<String, Object> delete(Tarj_Circulacion tar);
+    public Map<String, Object> delete(int id);
     public Map<String, Object> read(int id);
     public Map<String, Object> readAll();
+    public Map<String, Object> buscar(int nro);
+    public Map<String, Object> listest(int estado);
+    
+    
+    
 }
