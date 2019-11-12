9ac872101a809c5139c594173a237d244139f55c
unknown <richeti1998@gmail.com>
2019-11-11 19:59:33 -0500

    vin

--- src/main/java/com/musa2/controller/ConductoresController.java
+++ src/main/java/com/musa2/controller/ConductoresController.java
@@ -3,6 +3,7 @@ package com.musa2.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.musa2.entity.Conductores;
 import com.musa2.service.ConductoresService;
 
+@CrossOrigin ("*")
 @RestController
 @RequestMapping("/conductores")
 public class ConductoresController {
