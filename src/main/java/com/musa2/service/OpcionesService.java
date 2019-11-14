5df1164fc1ee372984cf272d144e3f120761cfa7
Samuel251220 <37707542+Samuel251220@users.noreply.github.com>
2019-11-14 11:00:03 -0500

    mensajito

--- src/main/java/com/musa2/service/OpcionesService.java
+++ src/main/java/com/musa2/service/OpcionesService.java
@@ -6,7 +6,7 @@ import com.musa2.entity.Opciones;
 public interface OpcionesService {
 	public Map<String,Object> create(Opciones o);
 	public Map<String,Object> update(Opciones o);
-	public int delete(int id);
+	public Map<String,Object> delete(Opciones o);
 	public Map<String,Object> read(int id);
 	public Map<String,Object> readn(int nombre);
 	public Map<String,Object> reade(int estado);
